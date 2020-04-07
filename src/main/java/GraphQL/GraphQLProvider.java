fe07e149d596532b5591aa3be739d7b78777c8ba
Aleksandr Khorev <aleks.k@tonlabs.io>
2020-04-07 12:15:37 +0300

    added graphq

---
+++ src/main/java/GraphQL/GraphQLProvider.java
@@ -0,0 +1,38 @@
+package com.graphqljava.tutorial.javademo;
+
+@Component
+public class GraphQLProvider {
+    private GraphQl graphQL;
+
+    @Bean
+    public GraphQl graphQL() {
+        return graphQL;
+    }
+
+    @PostConstruct
+    public void init() throws IOException {
+        URL url = Resources.getResource("schema.graphqls");
+        String sdl = Resources.toString(url, Charsets.UTF_8);
+        GraphQLSchema graphQLSchema = buildSchema(sdl);
+        this.graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+    }
+
+    @Autowired
+    GraphQLDataFetchers graphQLDataFetchers;
+
+    private GraphQLSchema buildSchema(String sdl) {
+        TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(sdl);
+        RuntimeWiring runtimeWiring = buildWiring();
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        return schemaGenerator.makeExecutableSchema(typeRegistry, runtimeWiring);
+    }
+
+    private RuntimeWiring buildWiring() {
+        return RuntimeWiring.newRuntimeWiring()
+                .type(newTypeWiring("Query")
+                        .dataFetcher("bookById", graphQLDataFetchers.getBookByIdDataFetcher()))
+                .type(newTypeWiring("Book")
+                        .dataFetcher("author", graphQLDataFetchers.getAuthorDataFetcher()))
+                .build();
+    }
+}
---
+++ src/test/java/com/graphqljava/tutorial/bookdetails/JavaDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.graphqljava.tutorial.javademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemoTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
